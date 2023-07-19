e54fa13d532be8594f6467ac00f1fa264b5edfba
Massimo Lolli <massimo.lolli@comtradeintegration.onmicrosoft.com>
2023-07-19 12:44:43 +0200

    Changed script and field types

--- src/Cart.svelte
+++ src/Cart.svelte
@@ -1,6 +1,8 @@
 <script>
     function track_msdynmkt_testtrigger_090959945() {
-        window["msdynmkt"].setUser({ authId: "lollim@comtrade.com"});   // ID, e-mail or phone number - see instructions
+
+        
+        window["msdynmkt"].setUser({ authId: "mlolli@comtrade.com"});   // ID, e-mail or phone number - see instructions
         window["msdynmkt"].trackEvent({
             name: "msdynmkt_testtrigger_090959945", //Trigger title: Test trigger
             ingestionKey : "d526b9ddda844126893515798c13fc49-bc8ca8d2-f6f6-4e69-b6f8-59c361488449-7573",
@@ -18,6 +20,8 @@
                 "bindingid" : obj.bindingid
             }
         });
+
+        document.querySelectorAll('input').forEach((e) => { e.value = '' });
     }
 
     export let obj = {
@@ -36,15 +40,15 @@
 
 <h1>Cart Script</h1>
 
-<input type="text" name="date" placeholder="date" id="boh"   bind:value={obj.date} />
+<input type="datetime-local" name="date" placeholder="date" id="boh"   bind:value={obj.date} />
 <input type="text" name="fname" placeholder="fname" id="boh2" bind:value={obj.fname} />
 <input type="text" name="lname" placeholder="lname" id="boh3" bind:value={obj.lname} />
 <input type="text" name="laststep" placeholder="laststep" id="boh4" bind:value={obj.laststep} />
-<input type="text" name="productid" placeholder="productid" id="boh5" bind:value={obj.productid} />
+<input type="number" name="productid" placeholder="productid" id="boh5" bind:value={obj.productid} />
 <input type="text" name="productname" placeholder="productname" id="boh6" bind:value={obj.productname} />
 <input type="text" name="state" placeholder="state" id="boh7" bind:value={obj.state} />
 <input type="text" name="totalamount" placeholder="totalamount" id="boh8" bind:value={obj.totalamount} />
 <input type="text" name="url" placeholder="url" id="boh9" bind:value={obj.url} />
 <input type="text" name="bindingid" placeholder="bindingid" id="boh10" bind:value={obj.bindingid} />
 
-<button on:click={() => track_msdynmkt_testtrigger_090959945()}>Test Trigger Script</button>
+<button on:click={() => track_msdynmkt_testtrigger_090959945() }>Test Trigger Script</button>
