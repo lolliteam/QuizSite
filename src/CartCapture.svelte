0bd8be96d59ad478f44cb28330beaeb7df8dfa70
Massimo Lolli <massimo.lolli@comtradeintegration.onmicrosoft.com>
2023-08-08 11:12:53 +0200

    che pallllleeeeeeeeeee

--- src/CartCapture.svelte
+++ src/CartCapture.svelte
@@ -9,39 +9,32 @@
 <script>
     function formSubmit() {
         //document.querySelector('#submitForm3').click();
-        MsCrmMkt.MsCrmFormLoader.sendFormCaptureToCrm(document.getElementById('fakeform3'))
+        MsCrmMkt.MsCrmFormLoader.sendFormCaptureToCrm(document.getElementById('fakeform3'));
+
+        window["msdynmkt"].setUser({ authId: obj.email});   // ID, e-mail or phone number - see instructions
+                
+        console.log(obj)
+        
+        window["msdynmkt"].trackEvent({
+            name: "msdynmkt_testtrigger_090959945", //Trigger title: Test trigger
+            ingestionKey : "d526b9ddda844126893515798c13fc49-bc8ca8d2-f6f6-4e69-b6f8-59c361488449-7573",
+            version: "1.0.0" ,
+            properties: {
+                "date" : obj.date,
+                "firstname" : obj.fname,
+                "lastname" : obj.lname,
+                "laststep" : obj.laststep,
+                "productid" : obj.productid,
+                "productname" : obj.productname,
+                "state" : obj.state,
+                "totalamount" : obj.totalamount,
+                "url" : obj.url,
+                "bindingid" : obj.bindingid
+            }
+        });
+
+        document.querySelectorAll('input').forEach((e) => { e.value = '' });
     }
-
-    MsCrmMkt.MsCrmFormLoader
-        .on("afterFormSubmit", function(event) {
-            console.log(event);
- 
-                window["msdynmkt"].setUser({ authId: obj.email});   // ID, e-mail or phone number - see instructions
-                
-                console.log(obj)
-                
-                window["msdynmkt"].trackEvent({
-                    name: "msdynmkt_testtrigger_090959945", //Trigger title: Test trigger
-                    ingestionKey : "d526b9ddda844126893515798c13fc49-bc8ca8d2-f6f6-4e69-b6f8-59c361488449-7573",
-                    version: "1.0.0" ,
-                    properties: {
-                        "date" : obj.date,
-                        "firstname" : obj.fname,
-                        "lastname" : obj.lname,
-                        "laststep" : obj.laststep,
-                        "productid" : obj.productid,
-                        "productname" : obj.productname,
-                        "state" : obj.state,
-                        "totalamount" : obj.totalamount,
-                        "url" : obj.url,
-                        "bindingid" : obj.bindingid
-                    }
-                });
-
-
-                document.querySelectorAll('input').forEach((e) => { e.value = '' });
-                return false;            
-        })
 
     let obj = {
         email : '',
