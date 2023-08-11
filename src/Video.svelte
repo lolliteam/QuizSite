bbef70ce7e9f72e840f1dfeacfe19b68d019af09
Massimo Lolli <massimo.lolli@comtradeintegration.onmicrosoft.com>
2023-08-11 10:49:52 +0200

    reformatted video

--- src/Video.svelte
+++ src/Video.svelte
@@ -1,5 +1,9 @@
 <div style="position: absolute;">
-    <video class="vjs-tech" preload="auto" muted="" autoplay loop playsinline="" id="my-video_html5_api" loading="lazy" poster="https://comtradeintegration.com/wp-content/uploads/2023/06/home-video-poster-mob.webp" style="position: absolute; width: 1519px; height: 854.438px; transform: translate3d(0px, -48px, 0px);">
-        <source type="video/mp4" src="https://comtradeintegration.com/wp-content/themes/integration/images/home-video.mp4">
-      </video>
+    <video class="vjs-tech" preload="auto" 
+           muted="" autoplay loop playsinline="" id="my-video_html5_api" loading="lazy"
+           poster="https://comtradeintegration.com/wp-content/uploads/2023/06/home-video-poster-mob.webp"
+           style="position: absolute; width: 1519px; height: 854.438px; transform: translate3d(0px, -48px, 0px);"
+    >
+      <source type="video/mp4" src="https://comtradeintegration.com/wp-content/themes/integration/images/home-video.mp4">
+    </video>
 </div>
